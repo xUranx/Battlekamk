b7e93323219cdb72519c1dcbb7bb4923ae98818f
PatrikBro <PatrikBro@AOTA1-FYSLA-04D>
2017-05-05 11:29:11 +0300

    kk

--- Game.cpp
+++ Game.cpp
@@ -5,7 +5,6 @@
 #include "Joystick.h"
 Game::Game(S1D13700 *handler, Grid grid, Joystick *stick): _handler(handler),_grid(grid),_stick(stick)
 {
-	//Radio::radioSetup();
 	if (PLAYER == 1)
 	{
 		_turn = true;
@@ -24,6 +23,8 @@ Game::~Game()
 void Game::
 gameloop(States gametype)
 {
+	Radio radio;
+	radio.radioSetup();
 	Coordinates coo;
 	coo.init(_handler, _stick);
 	coo.drawCoord();
@@ -33,24 +34,34 @@ gameloop(States gametype)
 		int y = 0;
 		if (_turn)
 		{
+#ifdef DEBUG = 0
+			Serial.println("It it your turn");
+#endif // DEBUG = 0
+
 			do
 			{
 			coo.coordLoop(x, y);
-
-			} while (_grid.chekValue(x,y) == Grid::Node::RESERVED);
+			} while (_grid.chekValue(x,y) == Grid::Node::RESERVED );
 			_grid.setValue(x, y, Grid::Node::SHOT);
-			//Radio::sendRadio(x, y);
 			int hit = 0;
-			int irrelevant = 0;
-			//Radio::listenRadio(hit,irrelevant);
+			int wincheck = 0;
+			if (gametype == States::PVP)
+			{
+			radio.sendRadio(x, y);
+			radio.listenRadio(hit, wincheck);
 #ifdef DEBUG = 0
 			Serial.print("Hit and irrelevant was ");
 			Serial.print(hit);
 			Serial.print("   ");
 			Serial.println("irrelevant ");
 #endif // DEBUG = 0
+			}
 			if (hit == 1)
 			{
+				if (wincheck == 1)
+				{
+					break; // WON
+				}
 				//BOOM BUUM RÄJÄHTI
 				_turn == true;
 			}
@@ -59,6 +70,41 @@ gameloop(States gametype)
 				_turn == false;
 			}
 
+		}
+		else
+		{
+#ifdef DEBUG = 0
+			Serial.println("It it enemy turn");
+#endif // DEBUG = 0
+			int x = 0;
+			int y = 0;
+			if (gametype == States::PVP)
+			{
+				
+				radio.listenRadio(x, y);
+				if (_grid.chekValue(x,y) == Grid::Node::BOAT)
+				{
+					_turn = false;
+					_grid.setValue(x, y, Grid::Node::FREE);
+					// UNDRAW BOX AND BOOM BOOM
+					if (_grid.checkVictory())
+					{
+						radio.sendRadio(1, 1);
+					}
+					else
+					{
+					radio.sendRadio(1, 0);
+					}
+				}
+				else
+				{
+					_turn = true;
+				}
+			}
+			else
+			{
+			
+			}
 		}
 
 	} while (!_grid.checkVictory());
